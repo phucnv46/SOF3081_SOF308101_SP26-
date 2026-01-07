9071f641e10499b5e029543dcb917063913d6a63
phucnv46 <phucnv46@fpt.edu.vn>
2026-01-07 09:12:27 +0700

    Add project files.

---
+++ buoi1/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+import MyMenu from './components/MyMenu.vue';
+
+const number = ref(0);
+const strInput = ref('chuỗi ban đầu');
+const mode = ref('vertical');
+</script>
+
+<template>
+  <h1 v-if="number >= 10">You did it!</h1>
+  <h1 v-else>Nhỏ hơn 10</h1>
+  <p>{{ number }}</p>
+
+  <button @click="number++">Tăng</button>
+  <button @click="number--">Giảm</button>
+
+  <hr />
+
+  <input v-model="strInput" type="text">
+  <p>Bạn đã nhập: {{ strInput }}</p>
+
+  <hr />
+
+  <label>Chọn hướng menu: </label>
+  <select v-model="mode">
+    <option value="horizontal">Ngang</option>
+    <option value="vertical">Dọc</option>
+  </select>
+
+  <MyMenu :mode="mode"></MyMenu>
+</template>
---
+++ buoi1/src/components/MyMenu.vue
@@ -0,0 +1,32 @@
+<template>
+    <a-menu :items="menuItems" :mode="props.mode" theme="dark"></a-menu>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps(['mode'])
+
+const menuItems = ref([
+    {
+        key: "dm1",
+        title: 'danh mục 1',
+        label: 'danh mục 1',
+
+    },
+    {
+        key: "dm2",
+        title: 'danh mục 2',
+        label: 'danh mục 2',
+
+    },
+    {
+        key: "dm3",
+        title: 'danh mục 3',
+        label: 'danh mục 3',
+
+    },
+])
+</script>
+
+<style scoped></style>
