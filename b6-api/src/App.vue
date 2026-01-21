9458fed4aeb660af69305175aaeebf7c167050ff
phucnv46 <phucnv46@fpt.edu.vn>
2026-01-21 09:04:55 +0700

    add post

--- b6-api/src/App.vue
+++ b6-api/src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import Table from 'ant-design-vue/es/table/Table';
 import axios from 'axios';
+import { message } from 'ant-design-vue';
 
-const products = ref(null);
-
+const posts = ref([]);
+const post = reactive(
+  {
+    id: -1,
+    title: '',
+    views: 0,
+  }
+)
 
 const columns = [
   {
@@ -15,12 +22,38 @@ const columns = [
     dataIndex: 'title',
     title: 'title'
   },
+  {
+    dataIndex: 'views',
+    title: 'views'
+  },
 ]
 
 const fetchData = () => {
-  axios.get('https://jsonplaceholder.typicode.com/posts').then(
-    res => products.value = res.data
+  axios.get('http://localhost:3000/posts'.trim()).then(
+    res => posts.value = res.data
   )
+}
+
+const addPost = () => {
+  const newId = 1;
+  if (posts.value.length === 0) {
+    newId = 1;
+  } else {
+    newId = Math.max(...posts.value.map(x => x.id)) + 1;
+  }
+  post.id = newId
+  axios.post('http://localhost:3000/posts', { ...post }).then(
+    res => console.log(res)
+  )
+
+  fetchData();
+  resetForm()
+}
+
+const resetForm = () => {
+  post.id = -1
+  post.title = '',
+    post.views = 0
 }
 
 onMounted(() => {
@@ -31,9 +64,27 @@ onMounted(() => {
 
 <template>
 
-  <a-table :dataSource="products" :columns="columns">
+
+  <a-form :model="post" :labelCol="{ offset: 4, span: 2 }" :wrapperCol="{ span: 12 }">
+    <a-form-item label="title" name="title" required :rules="[{ required: true, message: 'Không được để trống' }]">
+      <a-input v-model:value="post.title"></a-input>
+    </a-form-item>
+    <a-form-item label="views">
+      <a-input-number v-model:value="post.views" :min="0"></a-input-number>
+    </a-form-item>
+  </a-form>
+
+  <a-row style="margin-bottom: 20px;">
+    <a-col :offset="6">
+      <a-button type="primary" @click.prevent.stop="addPost">Thêm</a-button>
+    </a-col>
+  </a-row>
+
+  <a-table :dataSource="posts" :columns="columns">
 
   </a-table>
+
+
 
 </template>
 
