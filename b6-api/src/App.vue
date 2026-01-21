b800be6d0b53a6465dc28fdd274577acc5790e26
phucnv46 <phucnv46@fpt.edu.vn>
2026-01-21 08:12:18 +0700

    upload

---
+++ b6-api/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import Table from 'ant-design-vue/es/table/Table';
+import axios from 'axios';
+
+const products = ref(null);
+
+
+const columns = [
+  {
+    dataIndex: 'id',
+    title: 'id'
+  },
+  {
+    dataIndex: 'title',
+    title: 'title'
+  },
+]
+
+const fetchData = () => {
+  axios.get('https://jsonplaceholder.typicode.com/posts').then(
+    res => products.value = res.data
+  )
+}
+
+onMounted(() => {
+  fetchData()
+})
+
+</script>
+
+<template>
+
+  <a-table :dataSource="products" :columns="columns">
+
+  </a-table>
+
+</template>
+
+<style scoped></style>
